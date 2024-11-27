68125579a779a039e170144d38d6beeb82da27c6
Eddie Taliaferro II <eddielacrosse2@gmail.com>
2024-11-27 17:27:13 -0500

    Added Texture images for Quest Headset

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -65,68 +65,6 @@
  * - #7 Project Description Popover
  */
 
-
-
-// #3 Landing Header SVG Animation 
-fetch(headerArtPic)
-.then(response => response.text())
-.then(svgContent => {
-        // Process the SVG content
-        // https://g.co/bard/share/ec7f38615b0c
-        const svgContainer = document.getElementById('header-pic');
-        // svgContainer.style.width = "50%";
-        svgContainer.innerHTML = svgContent;
-        const lineOne = svgContainer.firstElementChild.childNodes[7] as HTMLElement;
-        const lineTwo = svgContainer.firstElementChild.childNodes[5] as HTMLElement;
-        const lineThree = svgContainer.firstElementChild.childNodes[3] as HTMLElement;
-        const lineFour = svgContainer.firstElementChild.childNodes[1] as HTMLElement;
-        
-        setInterval(() => {
-            lineOneAnim();
-            lineTwoAnim();
-            lineThreeAnim();
-            lineFourAnim();
-        }, 1600);
-        
-        function lineOneAnim() {
-            setTimeout(() => {
-                lineOne.style.fill = '#f3f3f3';
-            }, 200);
-            setTimeout(() => {
-                lineOne.style.fill = '#888';
-            }, 900);
-        }
-        function lineTwoAnim() {
-            setTimeout(() => {
-                lineTwo.style.fill = '#ffd24d';
-            }, 400);
-            setTimeout(() => {
-                lineTwo.style.fill = '#666';
-            }, 1000);
-        }
-        function lineThreeAnim() {
-            setTimeout(() => {
-                lineThree.style.fill = '#f56f33';
-            }, 600);
-            setTimeout(() => {
-                lineThree.style.fill = '#444';
-            }, 1100);
-        }
-        function lineFourAnim() {
-            setTimeout(() => {
-                lineFour.style.fill = '#f73209';
-            }, 800);
-            setTimeout(() => {
-                lineFour.style.fill = '#222';
-            }, 1200);
-        }
-
-        lineOne.style.transition = '0.5s';
-        lineTwo.style.transition = '0.5s';
-        lineThree.style.transition = '0.5s';
-        lineFour.style.transition = '0.5s';
-});
-
 // #5 Generate Projects UI
 interface Project {
   name: string,
@@ -159,6 +97,7 @@ function toggleBrightnessMode() {
     }
 }
 getBrightnessMode();
+
 
   /**
    * TailwindCSS Classes
@@ -220,6 +159,16 @@ getBrightnessMode();
       observer.observe(section);
     });
     }
+
+    // #3 Model Viewer Landing Header SVG Animation 
+    const modelViewerParameters = document.querySelector("model-viewer#xr-devices-hero");
+    modelViewerParameters.addEventListener("load", (ev) => {
+      // TODO: When this loads, hide loading
+      // TODO: Add loading spinner
+      let material = modelViewerParameters['model'].materials[0];
+      console.log(material);
+      
+    })
 
     // #7 Handle Project Description Popover
     /**
@@ -323,10 +272,20 @@ getBrightnessMode();
     <!-- Landing Intro -->
     <section data-current-section="s1">
       <div id="section-1" class={brightnessMode === "LIGHT" ? lightBG : darkBG}>
-        <div class="md:w-4/5 lg:w-3/5 mx-auto lg:flex flex-row gap-4">
+        <div class="md:w-4/5 lg:w-3/5 mx-auto lg:flex flex-row gap-20">
           <!-- Picture -->
           <div class="w-11/12 h-96 lg:w-2/4 mx-auto bg-[#9999990] content-center">
-            <model-viewer class="mx-auto scale-150" auto-rotate src="https://ik.imagekit.io/je4p51xox/3D%20Models/hero-animation.glb?updatedAt=1732733617454" shadow-intensity="1" camera-controls></model-viewer>
+            <model-viewer 
+              id="xr-devices-hero"
+              class="mx-auto relative bottom-10" 
+              style="transform: scale(3);" 
+              auto-rotate 
+              rotation-per-second="50deg"
+              auto-rotate-delay="3000"
+              src="https://ik.imagekit.io/je4p51xox/3D%20Models/hero-animation.glb?updatedAt=1732733617454"
+              
+              >
+            </model-viewer>
           </div>
           <!-- Text -->
           <div class="w-11/12 lg:w-2/4 mx-auto bg-[#9999990]">
@@ -339,7 +298,7 @@ getBrightnessMode();
                 </b> 
               </span>
               <br>
-              <span class="sm:text-xl xl:text-xl block my-4">
+              <span class="sm:text-xl xl:text-lg block my-4">
                 <span class={brightnessMode === "LIGHT" ? lightText : darkText}>
                   As a versatile developer and designer, I specialize in turning innovative ideas into tangible software. 
                   From web applications to immersive XR experiences, my expertise spans web development, responsive design, CSS & SVG animation, UI/UX design, and even video game development using Unreal Engine. With a focus on creating high-performing and effective software, I've successfully delivered MVPs and beyond on numerous projects.
@@ -369,64 +328,7 @@ getBrightnessMode();
           </div>
           <!-- Photo Side -->
           <div class="w-11/12 lg:w-2/4 content-center">
-            <div id="default-carousel" class="relative w-full" data-carousel="slide">
-  <!-- Carousel wrapper -->
-  <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-       <!-- Item 1 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoOne} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-      <!-- Item 2 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoTwo} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-      <!-- Item 3 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoThree} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-      <!-- Item 4 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoFour} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-      <!-- Item 5 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoFive} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-      <!-- Item 6 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoSix} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-      <!-- Item 7 -->
-      <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <img src={slidePhotoSeven} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-      </div>
-  </div>
-  <!-- Slider indicators -->
-  <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-      <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-  </div>
-  <!-- Slider controls -->
-  <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-      <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-          </svg>
-          <span class="sr-only">Previous</span>
-      </span>
-  </button>
-  <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-      <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-          </svg>
-          <span class="sr-only">Next</span>
-      </span>
-  </button>
-            </div>
+           <h1>Put something here later</h1>
           </div>
         </div>
       </div>
