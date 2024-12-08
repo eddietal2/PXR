96d23de98324812f70163063a473a8fb0dc2b521
Eddie Taliaferro <eddielacrosse2@gmail.com>
2024-12-07 23:02:29 -0500

    Added Icons to HowWeDo, Projects, and About Us Page

--- sveltekit-app/src/routes/projects/+page.svelte
+++ sveltekit-app/src/routes/projects/+page.svelte
@@ -160,7 +160,11 @@ getBrightnessMode();
     <section data-current-section="s1">
       <div id="section-1" class={brightnessMode === "LIGHT" ? lightBG : darkBG}>
         <div class="md:w-4/5 lg:w-3/5 mx-auto text-center">
-          <div class="h-40 w-40 bg-[#999] mx-auto"></div>
+          <div class="h-40 w-40 mx-auto">
+            <svg viewBox="0 0 24 24" fill="#00CF68" xmlns="http://www.w3.org/2000/svg">
+              <path  d="M18.6 3H5.4A2.4 2.4 0 0 0 3 5.4v13.2A2.4 2.4 0 0 0 5.4 21h13.2a2.4 2.4 0 0 0 2.4-2.4V5.4A2.4 2.4 0 0 0 18.6 3Z" fill="#00CF68" fill-opacity=".16" stroke="#00CF68" stroke-width="1.5" stroke-miterlimit="10"/><path d="M12 12V6M7 14V6M17 16V6" stroke="#00CF68" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"/>
+              </svg>
+          </div>
           <!-- <img 
           class="w-1/2 mx-auto" 
           alt="PXR Text Logo"
